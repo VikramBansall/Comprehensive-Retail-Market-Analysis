5a5945abf3a0447b87c5ef4b5b46c69a789cc1cd
Vikram Bansal <104368366+VikramBansall@users.noreply.github.com>
2023-11-16 12:20:16 +0530

    Add files via upload

---
+++ Retail-Analysis.R
@@ -0,0 +1,88 @@
+library(arules)
+library(arulesViz)
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+library(knitr)
+library(readxl)
+library(plyr)
+library(RColorBrewer)
+#
+#data<-read.csv("Retail.csv")
+retail<-read_excel("C:/Users/Vikra/OneDrive/Documents/Retaill.xlsx")
+retail
+View(retail)
+head(retail)
+#read excel into R dataframe
+#complete.cases(data) will return a logical vector indicating which rows have no missing values. Then use the vector to get only rows that are complete using retail[,].
+retail <- retail[complete.cases(retail), ]
+#mutate function is from dplyr package. It is used to edit or add new columns to dataframe. Here Description column is being converted to factor column. as.factor converts column to factor column. %>% is an operator with which you may pipe values to another function or expression
+retail %>% mutate(Description = as.factor(Description))
+retail %>% mutate(Country = as.factor(Country))
+#Converts character data to date. Store InvoiceDate as date in new variable
+retail$Date <- as.Date(retail$InvoiceDate)
+#Extract time from InvoiceDate and store in another variable
+TransTime<- format(retail$InvoiceDate,"%H:%M:%S")
+#Convert and edit InvoiceNo into numeric
+InvoiceNo <- as.numeric(as.character(retail$InvoiceNo))
+#Bind new columns TransTime and InvoiceNo into dataframe retail
+cbind(retail,TransTime)
+cbind(retail,InvoiceNo)
+#get a glimpse of your data
+glimpse(retail)
+#ddply(dataframe, variables_to_be_used_to_split_data_frame, function_to_be_applied)
+transactionData <- ddply(retail,c("InvoiceNo","Date"),
+                         function(df1)paste(df1$Description,
+                                            collapse = ","))
+#The R function paste() concatenates vectors to character and separated results using collapse=[any optional charcater string ]. Here ',' is used
+#set column InvoiceNo of dataframe transactionData  
+transactionData$InvoiceNo <- NULL
+#set column Date of dataframe transactionData
+transactionData$Date <- NULL
+#Rename column to items
+colnames(transactionData) <- c("items")
+#Show Dataframe transactionData
+transactionData
+write.csv(transactionData,"D:/Documents/market_basket_transactions.csv", quote = FALSE, row.names = FALSE)
+#transactionData: Data to be written
+#"D:/Documents/market_basket.csv": location of file with file name to be written to
+#quote: If TRUE it will surround character or factor column with double quotes. If FALSE nothing will be quoted
+#row.names: either a logical value indicating whether the row names of x are to be written along with x, or a character vector of row names to be written.
+tr <- read.transactions('D:/Documents/market_basket_transactions.csv', format = 'basket', sep=',')
+#sep tell how items are separated. In this case you have separated using ','
+`trObj<-as(dataframe.dat,"transactions")`
+tr
+summary(tr)
+# Create an item frequency plot for the top 20 items
+if (!require("RColorBrewer")) {
+  # install color package of R
+  install.packages("RColorBrewer")
+  #include library RColorBrewer
+  library(RColorBrewer)
+}
+itemFrequencyPlot(tr,topN=20,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
+itemFrequencyPlot(tr,topN=20,type="absolute")
+itemFrequencyPlot(tr,topN=20,type="relative",col=brewer.pal(8,'Pastel2'),main="Relative Item Frequency Plot")
+# Min Support as 0.001, confidence as 0.8.
+association.rules <- apriori(tr, parameter = list(supp=0.001, conf=0.8,maxlen=10))
+inspect(association.rules[1:10])
+shorter.association.rules <- apriori(tr, parameter = list(supp=0.001, conf=0.8,maxlen=3))
+subset.rules <- which(colSums(is.subset(association.rules, association.rules)) > 1) # get subset rules in vector
+length(subset.rules)  #> 3913
+metal.association.rules <- apriori(tr, parameter = list(supp=0.001, conf=0.8),appearance = list(default="lhs",rhs="METAL"))
+# Here lhs=METAL because you want to find out the probability of that in how many customers buy METAL along with other items
+inspect(head(metal.association.rules))
+metal.association.rules <- apriori(tr, parameter = list(supp=0.001, conf=0.8),appearance = list(lhs="METAL",default="rhs"))
+# Here lhs=METAL because you want to find out the probability of that in how many customers buy METAL along with other items
+inspect(head(metal.association.rules))
+# Filter rules with confidence greater than 0.4 or 40%
+subRules<-association.rules[quality(association.rules)$confidence>0.4]
+#Plot SubRules
+plot(subRules)
+
+plot(subRules,method="two-key plot")
+
+#See help("Deprecated")plot: Too many rules supplied. Only plotting the best 1000 rules using measure lift (change parameter max if needed)To reduce overplotting, jitter is added! Use jitter = 0 to prevent jitter.
+top10subRules <- head(subRules, n = 10, by = "confidence")
+plot(top10subRules, method = "graph",  engine = "htmlwidget")
+
